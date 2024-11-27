68e9bf3fb5d0017b9b9627c9c6c1eb970f1a81a1
Lans109 <lanceromero1029@gmail.com>
2024-11-27 17:33:05 +0800

    reference when not logged in change

--- End-Users/students/student_dashboard.php
+++ End-Users/students/student_dashboard.php
@@ -5,7 +5,7 @@ include('../db/databasecon.php');
 
 // Check if the user is logged in
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
-    header("Location: login.php");
+    header("Location: index.php");
     exit();
 }
 
