74683d4909f225d2ab1073d6bed9879be68de3f7
Lans109 <lanceromero1029@gmail.com>
2024-11-28 23:25:47 +0800

    added dashboard.css to href

--- End-Users/students/student_dashboard.php
+++ End-Users/students/student_dashboard.php
@@ -72,6 +72,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../Styles/styles.css">
+    <link rel="stylesheet" href="../Styles/dashboard.css">
     <title>Student Dashboard</title>
 </head>
 <body>
